5d018aac1611edd99df678b5e9a24014bf17c81b
Ankit Thakur <ankit@wealthtech.in>
2025-05-18 16:58:06 +0530

    strict file name check

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom';
-import Header from './Header';
-import Footer from './Footer';
+import Header from './header';
+import Footer from './footer';
 
 const Layout = () => {
   return (
-    <div className='flex flex-col min-h-screen'>
+    <div className='min-h-screen flex flex-col'>
       <Header />
-      <main className='flex-grow'>
+      <main className='flex-1'>
         <Outlet />
       </main>
       <Footer />
